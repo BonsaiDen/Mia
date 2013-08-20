8cd9a32d19616a20d6a60b9c5d8a41499cf73e97
Ivo Wetzel <ivo.wetzel@googlemail.com>
2013-08-20 08:03:56 +0200

    Make sure that parsers iterate over top level call and assignment expressions in the source

---
+++ test/parser/source/custom.test.js
@@ -0,0 +1,36 @@
+var test = require('../../test'),
+    validateSource = test.validateSource;
+
+
+// Tests ----------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+describe('Source: Custom Function', function() {
+
+    var nodeCounter = 0;
+    function custom(name, node) {
+        nodeCounter++;
+        if (node.type === 'FunctionDeclaration' && name === 'foo') {
+            return new test.mia.tags.FunctionTag(node.$uid, name, null, []);
+        }
+    }
+
+    var config = {
+        sources: [custom]
+    };
+
+    it('should execute custom parser functions and run over all call and assignment expressions, even in sequence expressions', function() {
+
+        var source = 'function foo(){} callMethod(); "Hello World"; e = 2 + 2; [1, 2]; "Foo", e = 1 + 1;';
+        validateSource(source, config, 'foo', {
+            type: 'Function',
+            name: 'foo',
+            comment: null,
+            params: []
+        });
+
+        test.assert.strictEqual(nodeCounter, 4);
+
+    });
+
+});
+
